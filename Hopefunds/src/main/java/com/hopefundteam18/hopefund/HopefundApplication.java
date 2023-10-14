0d89b61e0a2f0b35192cbed56a2197d3b0b03798
Qianlin99 <97335794+Qianlin99@users.noreply.github.com>
2023-10-14 14:47:46 +0800

    Add files via upload

---
+++ Hopefunds/src/main/java/com/hopefundteam18/hopefund/HopefundApplication.java
@@ -0,0 +1,13 @@
+package com.hopefundteam18.hopefund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopefundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopefundApplication.class, args);
+	}
+
+}
